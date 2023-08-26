d39eabfdac4ee5524e2b6d8e33825c624dc67fce
IdaDanuartha <danuart21@gmail.com>
2023-08-26 17:24:42 +0800

    make load more feature in portfolio page

--- app.js
+++ app.js
@@ -11,3 +11,37 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+
+$(document).ready(function() {
+    const itemLength = $(".port-item").length
+
+    $(".port-item").slice(0, 6).show()
+    $(".load-less").hide()
+
+    $(".load-more").on("click", function() {
+        $(".port-item:hidden").slice(0, 3).show();
+
+        if($(".port-item:hidden").length == 0) {
+            $(".load-more").hide();
+        }
+
+        if($(".port-item:visible").length == itemLength) {
+            $(".load-less").show()
+        }
+    })
+
+    $(".load-less").on("click", function() {
+        $(".port-item:visible").slice(0, itemLength).hide();
+        $(".port-item:hidden").slice(0, 6).show();
+
+        if($(".port-item:visible").length == 6) {
+            $(".load-less").hide();
+            $(".load-more").show()
+        }
+
+        // if($(".port-item:hidden").length == 6) {
+        //     $(".load-more").show()
+        // }
+    })
+})
